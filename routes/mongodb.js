62547ee0e70ecdb34173aa25b9f1b86227a7050d
Leon <liang.li@charleskeith.com>
2018-07-26 17:12:36 +0800

    mongodb

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -6,19 +6,19 @@ var url = 'mongodb://192.168.133.54:27017/server';
 var dbUrl = 'mongodb://192.168.133.54:27017/';
 
 // 创建数据库
-MongoClient.connect(url,function(err, db) {
-    if(err)
-        throw err;
+// MongoClient.connect(url,function(err, db) {
+//     if(err)
+//         throw err;
 
-    console.log('数据库已创建');
+//     console.log('数据库已创建');
 
-    var dbase = db.db("server");
-    dbase.createCollection('port', function(err, res) {
-        if(err) throw err;
-        console.log('集合已创建');
-        db.close();
-    });
-});
+//     var dbase = db.db("server");
+//     dbase.createCollection('port', function(err, res) {
+//         if(err) throw err;
+//         console.log('集合已创建');
+//         db.close();
+//     });
+// });
 
 // 插入数据
 MongoClient.connect(dbUrl,function(err, db) {
@@ -83,57 +83,57 @@ MongoClient.connect(dbUrl,function(err, db) {
 });
 
 // 删除数据
-MongoClient.connect(dbUrl,function(err, db) {
-    if(err)
-        throw err;
+// MongoClient.connect(dbUrl,function(err, db) {
+//     if(err)
+//         throw err;
 
-    var dbase = db.db("server");
-    var whereStr = {
-        'city': 'hangzhou'
-    }
-    dbase.collection('port').deleteMany(whereStr,function(err, res) {
-        if (err) {
-            throw err;
-            return;
-        }
-        console.log(res.result.n);
-        db.close();
-    });
-});
+//     var dbase = db.db("server");
+//     var whereStr = {
+//         'city': 'hangzhou'
+//     }
+//     dbase.collection('port').deleteMany(whereStr,function(err, res) {
+//         if (err) {
+//             throw err;
+//             return;
+//         }
+//         console.log(res.result.n);
+//         db.close();
+//     });
+// });
 
-分页查询
+// 分页查询
 
-MongoClient.connect(dbUrl,function(err, db) {
-    if(err)
-        throw err;
+// MongoClient.connect(dbUrl,function(err, db) {
+//     if(err)
+//         throw err;
 
-    var dbase = db.db("server");
-    var whereStr = {
+//     var dbase = db.db("server");
+//     var whereStr = {
 
-    }
-    dbase.collection('port').find(whereStr).limit(10).toArray(function(err, res) {
-        if (err) {
-            throw err;
-            return;
-        }
-        console.log(res);
-        db.close();
-    });
-});
+//     }
+//     dbase.collection('port').find(whereStr).limit(10).toArray(function(err, res) {
+//         if (err) {
+//             throw err;
+//             return;
+//         }
+//         console.log(res);
+//         db.close();
+//     });
+// });
 
-// 删除集合
-MongoClient.connect(dbUrl,function(err, db) {
-    if(err)
-        throw err;
+// // 删除集合
+// MongoClient.connect(dbUrl,function(err, db) {
+//     if(err)
+//         throw err;
 
-    var dbase = db.db("server");
-    dbase.collection('port').drop(function(err, res) {
-        if (err) {
-            throw err;
-        }
-        if(res) {
-            console.log("集合已删除");
-        }
-        db.close();
-    });
-});
+//     var dbase = db.db("server");
+//     dbase.collection('port').drop(function(err, res) {
+//         if (err) {
+//             throw err;
+//         }
+//         if(res) {
+//             console.log("集合已删除");
+//         }
+//         db.close();
+//     });
+// });
